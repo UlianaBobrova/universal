d82f438b6c42d6f7433e74db1bca7a0c9d0918d9
BOBROVA_ULIANA <uliana.bobrova@gmail.com>
2020-11-30 10:40:55 +0300

    create: videolessons

--- functions.php
+++ functions.php
@@ -19,7 +19,49 @@ if (!function_exists('universal_theme_setup')) :
             register_nav_menus( [
                 'header_menu' => 'Header menu',
                 'footer_menu' => 'Footer menu'
-            ] );
+			] );
+		// регистрация нового типа записей
+		add_action( 'init', 'register_post_types' );
+		function register_post_types(){
+		register_post_type( 'videolesson', [
+		'label'  => null,
+		'labels' => [
+			'name'               => 'Видеоуроки', // основное название для типа записи
+			'singular_name'      => 'Видеоурок', // название для одной записи этого типа
+			'add_new'            => 'Добавить Видеоурок', // для добавления новой записи
+			'add_new_item'       => 'Добавление Видеоурока', // заголовка у вновь создаваемой записи в админ-панели.
+			'edit_item'          => 'Редактирование Видеоурока', // для редактирования типа записи
+			'new_item'           => 'Новый Видеоурок', // текст новой записи
+			'view_item'          => 'Смотреть Видеоуроки', // для просмотра записи этого типа.
+			'search_items'       => 'Искать Видеоурок', // для поиска по этим типам записи
+			'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+			'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+			'parent_item_colon'  => '', // для родителей (у древовидных типов)
+			'menu_name'          => 'Видеоуроки', // название меню
+		],
+		'description'         => 'Раздел с видеоуроками',
+		'public'              => true,
+		// 'publicly_queryable'  => null, // зависит от public
+		// 'exclude_from_search' => null, // зависит от public
+		// 'show_ui'             => null, // зависит от public
+		// 'show_in_nav_menus'   => null, // зависит от public
+		'show_in_menu'        => true, // показывать ли в меню адмнки
+		// 'show_in_admin_bar'   => null, // зависит от show_in_menu
+		'show_in_rest'        => true, // добавить в REST API. C WP 4.7
+		'rest_base'           => null, // $post_type. C WP 4.7
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-video-alt2',
+		'capability_type'   => 'post',
+		//'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+		//'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+		'hierarchical'        => false,
+		'supports'            => [ 'title', 'editor', 'thumbnail', 'custom-fields' ], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+		'taxonomies'          => [],
+		'has_archive'         => true,
+		'rewrite'             => true,
+		'query_var'           => true,
+	] );
+}
    }
 endif;
 // все вышенаписанное сработает тогда,когда сработает хук событи 'after_setup_theme
---
+++ template-parts/content-videolesson.php
@@ -0,0 +1,114 @@
+<article id="videolesson-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <!-- шапка поста -->
+    <header class="entry-header <?php echo get_post_type()?>-header" style="background: linear-gradient(0deg, rgba(38, 45, 51, 0.75), rgba(38, 45, 51, 0.75)) ">
+        <div class="container">
+            <div class="videolesson-header-wrapper">
+                <div class="videolesson-header-nav">
+                    <?php
+		    foreach (get_the_category() as $category) {
+				printf(
+					'<a href="%s" class="category-link %s">%s</a>',
+					esc_url(get_category_link($category)),
+					esc_html( $category -> slug),
+					esc_html( $category -> name),
+				);
+			}
+			?>
+            </div>
+                <div class="videolesson">
+                <!-- <iframe width="100%" height="450" src="https://www.youtube.com/embed/a4fv-BtzNmY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+                    <?php
+                    $video_link = get_field('video');
+                    if(strpos($video_link, 'youtube')) {
+                        $tmp = explode('?v=', get_field('video'));?>
+                        <iframe width="100%" height="450" src="https://www.youtube.com/embed/<?php echo end($tmp); ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+                        <?php
+                    }
+                    if(strpos($video_link, 'vimeo')) {
+                        $tmp = explode('.com/', get_field('video'));?>
+                        <iframe src="https://player.vimeo.com/video/<?php echo end($tmp);?>" width="100%" height="640" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                        <?php
+                    }
+
+                         ?>
+                </div>
+                <div class="vdeolesson-header-title-wrapper">
+                
+                <div class="vdeolesson-header-item">
+                <div class="vdeolesson-header-left">
+                    <?php
+            // проверяем,точно ли мы странице поста
+		    if ( is_singular() ) :
+			    the_title( '<h1 class="post-title">', '</h1>' );
+		    else :
+			    the_title( '<h2 class="post-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		    endif;?>
+                    <!-- выводим краткое описание записи -->
+
+                    <!-- выводим  -->
+                    <div class="post-header-info">
+                        <span class="post-header-date">
+                            <img class="icon post-header-clock" src="<?php echo get_template_directory_uri() . '/assets/images/clock-icon.svg' ?>">
+                            <?php the_time('j F, H:i'); ?></span>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header><!-- .entry-header -->
+
+    <div class="container">
+    
+        <!-- Содержимое поста -->
+        <div class="post-content">
+            <?php
+        // выводим содержимое
+		the_content(
+            // выводим его на экран
+			sprintf(
+                // очищаем от разных лишних тегов 
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'universe-example' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+                // очищаем от разных лишних тегов title
+                wp_kses_post( get_the_title() )
+			)
+		);
+        // Обертка для страничной навигации, например,когда пост состоит из нескольких постов
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'universe-example' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+        
+        </div><!-- .post-content -->
+        </div>
+        <!-- подвал сайта -->
+        <footer class="post-footer">
+        <div class="container">
+            <?php 
+                $tags_list = get_the_tag_list( '', esc_html_x( ' ', 'list item separator', 'universe-example' ) );
+			    if ( $tags_list ) {
+				/* translators: 1: list of tags. */
+				    printf( '<span class="tags-links">' . esc_html__( '%1$s', 'universe-example' ) . '</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                }
+                // ссылки на соцсети
+                meks_ess_share(); ?>
+                </div>
+                <?php
+                // Виджет с рекомендованными статьями
+                // get_sidebar('page'); 
+            ?>
+
+        </footer>
+    
+</article>
